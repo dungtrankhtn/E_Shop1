a5f8b45e72a584be0749024d9cb3c28142b2fc96
Tran Quoc Dung <dungtran6665@gmail.com>
2017-11-21 14:23:59 +0700

    Up lại
    Đã hoàn thành login, admin, layouts

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', function () {
+    return view('frontpage.home');
+});
+Route::get('/login', function () {
+    return view('frontpage.login');
+});
+Route::get('/frontpage', function () {
+    return view('frontpage.layout.frontpage');
+});
