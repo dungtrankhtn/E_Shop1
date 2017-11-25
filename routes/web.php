4783adc483d52c1677b39292a448df516f816c90
TruongDuyIT <cutruongduy97@gmail.com>
2017-11-25 15:37:35 +0700

    Update Page Preview Product.
    Thêm phần get data cho trang chi tiết sản phẩm.

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,19 @@
 //    return view('welcome');
 //});
 
+
+//Route cho trang chủ
 Route::get('/', function () {
     return view('frontpage.home');
 });
 
+//Route get data cho trang sản phẩm (trang chủ).
 Route::resource('/', 'ProductController');
 
-// Route::resource('/preview', 'ProductController');
+//Route get data cho trang chi tiết sản phẩm (theo id).
+Route::get('preview/{id}', ['as'=>'preview','uses'=>'ProductController@preview']);
 
-
+//Route cho trang đăng nhập.
 Route::get('/login', function () {
     return view('frontpage.login');
 });
