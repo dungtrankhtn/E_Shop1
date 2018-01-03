addc28a9dbe93ff7d25fe465590399341f19332d
Tran Quoc Dung <dungtran6665@gmail.com>
2018-01-03 23:00:30 +0700

    chưa fix được css
    css fix

--- resources/views/frontpage/layout/frontpage.blade.php
+++ resources/views/frontpage/layout/frontpage.blade.php
@@ -12,6 +12,11 @@
     <script type="text/javascript" src="{{asset('js/easing.js')}}"></script>
     <link href='//fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
     <link href='//fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
+
+    <!-- For login -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
@@ -132,12 +137,38 @@
 
                     </script>
                 </div>
-                <div class="login">
-                    <span><a href="{{ url('/login') }}"><img src="{{asset('images/login.png')}}" alt="" title="login"/></a></span>
+                <div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a href="{{ route('login') }}"> <img src="{{asset('images/login.png')}}" alt="" title="login"> Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
                 </div>
                 <div class="clear"></div>
             </div>
             <div class="clear"></div>
+
         </div>
         <div class="h_menu">
             <a id="touch-menu" class="mobile-menu" href="#">Menu</a>
